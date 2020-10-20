691567a3cc443b15d686d61da8dc941b2caa3c11
梁亚根 <799383929@qq.com>
2020-10-20 17:33:07 +0800

    feat: render函数组件添加

--- app1/src/views/About.vue
+++ app1/src/views/About.vue
@@ -1,14 +1,35 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-10-08 10:17:04
+ * @LastEditTime: 2020-10-20 15:17:51
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /mic-service/app1/src/views/About.vue
+-->
 <template>
   <div class="home">
     <h1>我是 App1 About 页面</h1>
-    <img alt="Vue logo" src="../assets/logo.png">
+    <button @click="count">增加</button>
+    <Test :counts="counts" />
   </div>
 </template>
 
 <script>
+import Test from '../components/HelloWorld'
 export default {
   name: 'Home',
   components: {
+    Test
+  },
+  data () {
+    return {
+      counts: 0
+    }
+  },
+  methods: {
+    count() {
+      this.counts += 1
+    }
   }
 }
 </script>
